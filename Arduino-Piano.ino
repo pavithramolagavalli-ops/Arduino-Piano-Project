443a0237de58c6dedd2c27468f9aed4cb6c3061b
pavithramolagavalli-ops <pavithramolagavalli@gmail.com>
2026-06-02 23:03:16 +0530

    Create Arduino-Piano.ino

---
+++ Arduino-Piano.ino
@@ -0,0 +1,16 @@
+int buzzer = 8;
+void setup() {
+}
+void loop() {
+  tone(buzzer, 262); // Sa
+  delay(1000);
+
+  tone(buzzer, 294); // Re
+  delay(1000);
+
+  tone(buzzer, 330); // Ga
+  delay(1000);
+
+  tone(buzzer, 349); // Ma
+  delay(1000);
+}
